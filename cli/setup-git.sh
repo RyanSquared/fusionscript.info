87f7845279eadc74623ac59a3765f35bac5475b3
RyanSquared <vandor2012@gmail.com>
2017-04-27 00:01:03 -0500

    cli/: Initial commit

---
+++ cli/setup-git.sh
@@ -0,0 +1,20 @@
+sudo userdel -r git -f
+sudo useradd -m git
+sudo usermod -s $(command -v git-shell) git
+sudo loginctl enable-linger git
+cmd() {
+	echo "> $*"
+	sudo -u git $*
+}
+dir=~git/git-shell-commands
+cmd mkdir $dir
+pushd commands >/dev/null
+for file in *; do
+	cmd tee $dir/$file < $file
+	cmd chmod u+x $dir/$file
+done
+popd >/dev/null
+cmd mkdir ~git/.ssh
+echo "Please input the public key(s) used for authentication."
+echo "Press <C-D> when done."
+cmd tee ~git/.ssh/authorized_keys >/dev/null
