e401899c3704456443bde950ecbcf758b9032026
RyanSquared <vandor2012@gmail.com>
2017-04-30 10:06:19 -0500

    setup.py: Add bcrypt and sqlalchemy

--- web/setup.py
+++ web/setup.py
@@ -6,7 +6,7 @@ setup(
     name='fs-info',
     version='0.1.0-1',
     description='Website for displaying information abot stuff and things',
-    install_requires=['flask', 'tornado', 'pygments'],
+    install_requires=['flask', 'tornado', 'pygments', 'sqlalchemy', 'bcrypt'],
     author='Ryan',
     author_email='vandor2012@gmail.com',
     # url='https://github.com/RyanSquared/',
