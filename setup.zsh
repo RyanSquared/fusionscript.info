d7f707a41bfb8750bd07c796edfb14a6b1ecc3ee
RyanSquared <vandor2012@gmail.com>
2017-05-02 13:13:27 -0500

    setup.zsh: Add certbot and systemd automatic configuration

--- setup.zsh
+++ setup.zsh
@@ -5,14 +5,45 @@ setopt -x
 PYTHON=${PYTHON:=$(which python3)}
 PORT=${PORT:=25562}
 BINDHOST=${BINDHOST:=0.0.0.0}
+DOMAIN=${DOMAIN:=fusionscript.info}
 CERTFILE=${CERTFILE:=ssl/cert.pem}
 KEYFILE=${CERTFILE:=ssl/key.pem}
 DB_URI=${DB_URI:=sqlite://}
+HTTP_PORT=${HTTP_PORT:=25563}
+WITH_IPTABLES=${WITH_IPTABLES:=true}
+WITH_CERTBOT=${WITH_CERTBOT:=true}
+
+# Set up iptables rules
+if $WITH_IPTABLES; then
+	iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port ${PORT}
+	iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port ${HTTP_PORT}
+fi
 
 # Create the `git` user and install command line utilities
 pushd cli
 ./setup-git.zsh
 popd
+
+# Set up certbot
+if $WITH_CERTBOT; then
+	sudo cp etc/systemd/system/* /etc/systemd/system
+	sudo systemctl enable fs-serve-static-web.service
+	sudo systemctl enable fs-renew-certs.service
+	sudo systemctl enable fs-auto-renew-certs.timer
+	sudo tee /usr/local/bin/renew-certs-post < etc/bin/renew-certs-post
+	sudo chmod u+x /usr/local/bin/renew-certs-post
+	sudo systemctl start fs-serve-static-web.service
+	mkdir web
+	pushd web
+	$PYTHON -m http.server ${HTTP_PORT}
+	server_pid=$!
+	sudo certbot certonly --webroot -w $PWD -d ${DOMAIN}
+	kill $server_pid
+	popd
+	rmdir web
+	sudo systemctl start fs-auto-renew-certs.timer
+	sudo systemctl restart fs-renew-certs.service
+fi
 
 # Install the `fs-info` package
 pushd web
@@ -58,9 +89,11 @@ WantedBy=multi-user.target
 EOF
 
 
-# Copy over SSL files
-sudo -u git tee ~git/ssl/cert.pem >/dev/null < ${CERTFILE}
-sudo -u git tee ~git/ssl/key.pem >/dev/null < ${KEYFILE}
+# Copy over TLS files if certbot is not used
+if ! ${WITH_CERTBOT}; then
+	sudo -u git tee ~git/ssl/cert.pem >/dev/null < ${CERTFILE}
+	sudo -u git tee ~git/ssl/key.pem >/dev/null < ${KEYFILE}
+fi
 
 # Webserver configuration
 sudo -u git tee ~git/.config/fs-info/conf.json <<EOF
@@ -89,7 +122,5 @@ sudo chown -R git:git ~git/{templates,static}
 # Launch the website
 sudo systemctl enable fs-info-website.service
 sudo systemctl enable git-daemon.service
+sudo systemctl restart fs-info-website.service
 sudo systemctl restart git-daemon.service
-
-echo "Please make sure to run \`sudo systemctl enable fs-info-website.service\`"
-echo "after installing certificate and key to ~git/ssl"
